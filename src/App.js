9e700db60474f95dde88b87fd952d00f12851b77
LuigiPetrea <levipetrea08@gmail.com>
2022-10-25 19:13:41 +0300

    add front-end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
+import { useState, useRef } from 'react';
 import './App.css';
+import _ from "lodash";
 
 function App() {
-  return (
-    <div className="App">
+    const currentPlayer = useRef("X");
+    const click = (row, column) => {
+    let newXoState = _.cloneDeep(xoState);
+        newXoState[row][column] = currentPlayer.current;
+        setXoState(newXoState);
+        if (currentPlayer.current === "X"){
+            currentPlayer.current = "O";
+        }else{
+            currentPlayer.current = "X";
+        }
+    }
+    const [xoState, setXoState] = useState([
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ]
+
+    );
+
+    const generateCells = () => {
+        let cellsArray = [];
+        for(let row = 0; row < 3; ++row){
+            for (let column = 0; column < 3; ++column){
+                cellsArray.push(
+                    <div className='cell' style={{gridColumnStart: column + 1, gridRowStart: row + 1}} onClick= {() => click (row, column)}>
+                    {xoState[row][column]}
+                    </div> 
+                )
+            }
+        }
+        return cellsArray;
+    }
+    return (
+        <div className="App">
+            <h1 align="center">X & O</h1>
+        <div className='game'>
+        <div className="xoGrid">
+                {generateCells()}
+        </div>
+    </div>
     </div>
   );
 }
